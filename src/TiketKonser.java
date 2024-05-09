34ad471a21657b08241e1e30d32482b18c7ba436
Gilang Hafizh <149552530+gilanghafizh@users.noreply.github.com>
2024-05-09 21:28:10 +0700

    Update TiketKonser.java

--- src/TiketKonser.java
+++ src/TiketKonser.java
@@ -2,7 +2,7 @@ abstract class TiketKonser implements HargaTiket {
     public abstract double harga();
 
     @Override
-    public String toString(){
+    public String toString(){ //override method untuk mengembailkan nama kelas objek TiketKonser yang disederhanakan
         return getClass().getSimpleName();
     }
  }
